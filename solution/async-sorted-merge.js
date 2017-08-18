27e0739b9a5d5bd408724af5ad26d0cb1917db82
Michelle Dick <michelle.dick@nutanix.com>
2017-08-17 19:01:56 -0700

    Comment edits.

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -6,6 +6,11 @@ const SortUtil = require('./sort-util');
 // In the asynchronous case, when we pop an element from the LogSource Object,
 // we will see delays of up to 8 milliseconds.
 
+// NOTE: If editing of the log-source file functionality is allowed, it may be
+// be better to store the logs locally and pop them one after another, however
+// I have yet to find a good way to bind the source index of the log to the
+// resolution of the popAsync() function without editing the function itself..
+
 module.exports = (logSources, printer) => {
 	const promises = [];
 	// Generate series of promises that pop the next log from each logSource.
